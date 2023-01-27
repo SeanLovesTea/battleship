79ace695d76b23b26c6bc801ed2d9db2ad2caae5
SeanLovesTea <sean.m.concannon@gmail.com>
2023-01-27 12:49:56 +0000

    Created and test ship factory function

---
+++ main.js
@@ -0,0 +1,25 @@
+const shipMethods = {
+  randomFunction () {
+    return 'random function'
+  },
+  hit () {
+    this.hitCount++
+  }
+}
+
+function createShip (length) {
+  const ship = Object.create(shipMethods)
+  ship.length = length
+  ship.hitCount = 0
+  ship.isSunk = false
+  return ship
+}
+
+const smallShip = createShip(2)
+const medShip = createShip(3)
+const largeShip = createShip(5)
+console.log(smallShip.randomFunction())
+console.log(medShip)
+largeShip.hit()
+largeShip.hit()
+console.log(largeShip)
